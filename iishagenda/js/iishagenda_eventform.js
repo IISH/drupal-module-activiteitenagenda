f755f2a0975b82f756909d8743a045ba49f4868f
Crucq <info@pietercrucq.nl>
2015-07-21 14:00:00 +0200

    improved layout for catering page, focus on check availability button when date changes, changed permissions (role) set for event node views

--- iishagenda/js/iishagenda_eventform.js
+++ iishagenda/js/iishagenda_eventform.js
@@ -3,7 +3,7 @@
     jQuery(document).ready(function($) {
 
 
-        // clear rooms when none is checed (mainly for new events)
+        // clear rooms when none is checked (mainly for new events)
         if($('.field-name-field-event-room .form-checkbox:checked').length == 0){
             clear_rooms();
         }
@@ -14,17 +14,27 @@
         $('.form-item-field-event-date-und-0-value2-date').css("display","none");
         $("label[for='edit-field-event-date-und-0-value2']").html("tot");
 
-
         $('#edit-field-event-date-und-0-value-datepicker-popup-0').change(function() {
             $("#edit-field-event-date-und-0-value2-datepicker-popup-0").val($("#edit-field-event-date-und-0-value-datepicker-popup-0").val());
-            clear_rooms();
+            onchange_date();
         });
-        $('#edit-field-event-date-und-0-value-timeEntry-popup-1').change(clear_rooms);
-        $('#edit-field-event-date-und-0-value2-timeEntry-popup-1').change(clear_rooms);
+        $('#edit-field-event-date-und-0-value-timeEntry-popup-1').change(onchange_date);
+        $('#edit-field-event-date-und-0-value2-timeEntry-popup-1').change(onchange_date);
+
+
+        $('#edit-check').mousedown(function(){
+            $('#edit-check').css("border","0px");
+        });
+
+        function onchange_date(){
+            $('#edit-check').css("border","2px solid red");
+            clear_rooms();
+        }
 
         function clear_rooms(){
             $('.field-name-field-event-room .form-checkboxes').html("klik op 'Controleer beschikbaarheid' voor beschikbare zalen.");
         }
+
     });
 
 })(jQuery, Drupal, this, this.document);
